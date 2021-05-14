5acaa0520c8afdb58b6030f3e56e054f49be24f6
Y00ujin <yujingim43@gmail.com>
2021-05-14 20:52:04 +0900

    [Modify] In PortfolioViewController, HeaderView layout Completed with SnapKit

---
+++ OLIO/View/SearchTextFieldView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchTextFieldView.swift
+//  OLIO
+//
+//  Created by 김유진 on 2021/05/14.
+//
+
+import UIKit
+
+class SearchTextFieldView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
